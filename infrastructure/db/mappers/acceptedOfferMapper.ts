1186f778b57d59ceaaf4138a026db7bfb7fdfdae
Aleksei Monakhov <lexmonakhov@yandex.ru>
2023-09-11 11:17:22 +0530

    add accepted offers infrastucture db model and mapper

---
+++ infrastructure/db/mappers/acceptedOfferMapper.ts
@@ -0,0 +1,27 @@
+import { AcceptedOffer } from "../../../core/models/AcceptedOffer";
+import { AcceptedOfferDBModel } from "../models/acceptedOfferDBModel";
+
+interface DBMapper<D, E> {
+    toEntity(domain: D): E;
+    toDomain(entity: E): D;
+}
+
+export class AcceptedOfferMapper implements DBMapper<AcceptedOffer, AcceptedOfferDBModel> {
+    toDomain(entity: AcceptedOfferDBModel): AcceptedOffer {
+        return new AcceptedOffer(
+            entity.accepted_offer_id,
+            entity.offer_id,
+            entity.traffic_provider_id,
+            entity.proxy_link,
+        );
+    }
+
+    toEntity(domain: AcceptedOffer): AcceptedOfferDBModel {
+        return {
+            accepted_offer_id: domain.id,
+            offer_id: domain.offerId,
+            traffic_provider_id: domain.trafficProviderId,
+            proxy_link: domain.proxyLink,
+        };
+    }
+}
---
+++ infrastructure/db/models/acceptedOfferDBModel.ts
@@ -0,0 +1,6 @@
+export interface AcceptedOfferDBModel {
+    accepted_offer_id: number;
+    offer_id: number;
+    traffic_provider_id: number;
+    proxy_link: string;
+}
