6bacca643c303f21ff1d32207ff515d6959561dd
Aleksei Monakhov <lexmonakhov@yandex.ru>
2023-10-08 22:28:27 +0530

    add clientsTable

--- leadfin_frontend/src/components/pages/adminDashboard/adminDashboard.tsx
+++ leadfin_frontend/src/components/pages/adminDashboard/adminDashboard.tsx
@@ -1,14 +1,41 @@
-import React from 'react';
-import styles from './adminDashboard.module.css'
+import React, { useState, useEffect } from 'react';
+import Chart from '../clientDashboard/chart/chart';
+import Button from '@mui/material/Button';
+import styles from './adminDashboard.module.css';
+import ClientsTable from './clientTable/clientsTable';
 
-export default function AdminDashboard() {
+export default function ClientDashboard() {
+  const [activeTab, setActiveTab] = useState('statistics');
+
+  useEffect(() => {
+    const savedTab = localStorage.getItem('activeTab');
+    if (savedTab) {
+      setActiveTab(savedTab);
+    }
+  }, []);
+
+  const handleTabClick = (tab: string) => {
+    setActiveTab(tab);
+    localStorage.setItem('activeTab', tab);
+  };
+
   return (
-    <div className={styles.AdminDashboard}>
-      <header className={styles['AdminDashboard-header']}>
-        <a>
-          AdminDashboard will be here...
-        </a>
-      </header>
+    <div>
+      <div className={styles.adminContainer}>
+        <div className={styles.buttonContainer}>
+          <Button
+            className={`${styles.button} ${activeTab === 'Рекламодатели' ? styles.active : ''}`}
+            variant="text"
+            color="primary"
+            onClick={() => handleTabClick('clients')}
+          >
+            Рекламодатели
+          </Button>
+        </div>
+      </div>
+      <div>
+        {activeTab === 'clients' && <ClientsTable/>}
+      </div>
     </div>
   );
 }
---
+++ leadfin_frontend/src/components/pages/adminDashboard/clientTable/clientsTable.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import styles from './clientsTable.module.css';
+import { columns, dataFromDatabase } from './mockData';
+
+export default function ClientsTable() {
+  const [clients, setClients] = useState(dataFromDatabase);
+
+  return (
+    <div className={styles.container}>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell key={column}>{column}</TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {clients.map((row) => (
+              <TableRow
+                key={row.id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell>{row.companyName}</TableCell>
+                <TableCell>{row.email}</TableCell>
+                <TableCell>{row.contactPerson}</TableCell>
+                <TableCell>{row.commMethod}</TableCell>
+                <TableCell>{row.manager}</TableCell>
+                <TableCell>{row.created}</TableCell>
+                <TableCell>{row.offers}</TableCell>
+                <TableCell>{row.cap}</TableCell>
+                <TableCell>{row.dailyLimit}</TableCell>
+                <TableCell>{row.totalLimit}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+}
