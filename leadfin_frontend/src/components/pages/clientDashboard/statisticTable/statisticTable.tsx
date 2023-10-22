a6c384fd461d411622eeb28837bd84f68e247e3c
Aleksei Monakhov <lexmonakhov@yandex.ru>
2023-10-23 05:08:24 +0530

    remove unusable offer modal from client dashboard

--- leadfin_frontend/src/components/pages/clientDashboard/statisticTable/statisticTable.tsx
+++ leadfin_frontend/src/components/pages/clientDashboard/statisticTable/statisticTable.tsx
@@ -6,65 +6,14 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import Button from '@mui/material/Button';
-import AddIcon from '@mui/icons-material/Add';
-import EditIcon from '@mui/icons-material/Edit';
 import styles from './statisticTable.module.css';
 import { columns, dataFromDatabase } from './mockData';
-import AddOfferModal, { NewOffer }  from '../offerModal/addOfferModal';
-import EditOfferModal from '../offerModal/editOfferModal';
 
 export default function StatisticTable() {
-  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
-  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
-  const [offers, setOffers] = useState(dataFromDatabase);
-  const [selectedOffer, setSelectedOffer] = useState<NewOffer | null>(null);
-
-  const handleOpenAddModal = () => {
-    setIsAddModalOpen(true);
-    setSelectedOffer(null);
-  };
-
-  const handleCloseAddModal = () => {
-    setIsAddModalOpen(false);
-  };
-
-  const handleOpenEditModal = (offer: NewOffer) => {
-    setIsEditModalOpen(true);
-    setSelectedOffer(offer);
-  };
-
-  const handleCloseEditModal = () => {
-    setIsEditModalOpen(false);
-  };
-
-  const handleAddOffer = (newOffer: NewOffer) => {
-    const newOfferWithId = { ...newOffer, id: Date.now() };
-    setOffers([...offers, newOfferWithId]);
-    handleCloseAddModal();
-  };
-
-  const handleEditOffer = (editedOffer: NewOffer) => {
-    const index = offers.findIndex((o) => o.id === editedOffer.id);
-    if (index !== -1) {
-      const updatedOffers = [...offers];
-      updatedOffers[index] = editedOffer;
-      setOffers(updatedOffers);
-      handleCloseEditModal();
-    }
-  };
+  const [offers] = useState(dataFromDatabase);
 
   return (
     <div className={styles.container}>
-      <Button
-        variant="contained"
-        startIcon={<AddIcon />}
-        className={styles.addButton}
-        onClick={handleOpenAddModal}
-      >
-        Добавить оффер
-      </Button>
-
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
@@ -72,7 +21,6 @@ export default function StatisticTable() {
               {columns.map((column) => (
                 <TableCell key={column}>{column}</TableCell>
               ))}
-              <TableCell>Действия</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -91,32 +39,11 @@ export default function StatisticTable() {
                 <TableCell>{row.created}</TableCell>
                 <TableCell>{row.modified}</TableCell>
                 <TableCell>{row.clicks}</TableCell>
-                <TableCell>
-                  <Button
-                    variant="outlined"
-                    size="small"
-                    onClick={() => handleOpenEditModal(row)}
-                  >
-                    <EditIcon />
-                  </Button>
-                </TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
-
-      <AddOfferModal
-        open={isAddModalOpen}
-        onClose={handleCloseAddModal}
-        onAddOffer={handleAddOffer}
-      />
-      <EditOfferModal
-        open={isEditModalOpen}
-        onClose={handleCloseEditModal}
-        onEditOffer={handleEditOffer}
-        selectedOffer={selectedOffer}
-      />
     </div>
   );
 }
