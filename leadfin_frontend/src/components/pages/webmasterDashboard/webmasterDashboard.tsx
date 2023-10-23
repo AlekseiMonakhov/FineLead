ae2cc9bb9d7d3f6cf3c6f858fd87f3f9b69ba893
Aleksei Monakhov <lexmonakhov@yandex.ru>
2023-10-23 05:33:47 +0530

    change mock webmaster dashboard

--- leadfin_frontend/src/components/pages/webmasterDashboard/webmasterDashboard.tsx
+++ leadfin_frontend/src/components/pages/webmasterDashboard/webmasterDashboard.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import BuildIcon from '@mui/icons-material/Build';
 import styles from './webmasterDashboard.module.css'
 
 export default function WebmasterDashboard() {
-    return (
-      <div className={styles.WebmasterDashboard}>
-        <header className={styles['WebmasterDashboard-header']}>
-          <a>
-             WebmasterDashboard will be here...
-          </a>
-        </header>
-      </div>
-    );
-  }
+  return (
+    <div className={styles.MainPage}>
+      <header className={styles['Page-header']}>
+        <h3 className={styles.title}>LeadFin</h3>
+        <h1 className={styles.text}>Здесь будет панель Вебмастера</h1>
+        <BuildIcon className={styles.icon} />
+        <a className={styles.text}>Ведутся работы</a>
+      </header>
+    </div>
+  );
+}
